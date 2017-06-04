b5e7d0b12e337e3ee7cb4ff3642310149e575af0
tagasouichiro <stitow14@gmail.com>
2017-06-04 14:21:28 +0900

    Add read subscribe req

--- meta/handler.go
+++ meta/handler.go
@@ -1,6 +1,8 @@
 package meta
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -9,6 +11,17 @@ var (
 	subHandler = "/meta/subscribe"
 )
 
+type subscriptionReq struct {
+	Channels []string
+}
+
+type subscriptionRes struct {
+	Channel    string `json:"channel"`
+	Successful bool   `json:"successful"`
+	// ClientID     string `json:"clientId"`
+	Subscription string `json:"subscription"`
+}
+
 // Handler is meta channel definition
 func Handler() {
 	http.HandleFunc(subHandler, subscribe)
@@ -16,4 +29,29 @@ func Handler() {
 
 func subscribe(w http.ResponseWriter, r *http.Request) {
 	log.Println(subHandler)
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		unsuccess(subHandler, "", w)
+		return
+	}
+
+	subReq, err := json.Marshal(body)
+	if err != nil {
+		unsuccess(subHandler, "", w)
+		return
+	}
+}
+
+func unsuccess(channel string, subscritpion string, w http.ResponseWriter) {
+	res := subscriptionRes{
+		Channel:      subHandler,
+		Successful:   false,
+		Subscription: "",
+	}
+	json, err := json.Marshal(res)
+	if err != nil {
+		return
+	}
+	w.Write(json)
 }
