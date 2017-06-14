a80e48a9efcdb7eb5d5c23e0916751bd6e0513df
tagasouichiro <stitow14@gmail.com>
2017-06-14 23:30:49 +0900

    Add mail func

---
+++ topic/mail.go
@@ -0,0 +1,22 @@
+package topic
+
+import (
+	"net/smtp"
+)
+
+type Mail struct {
+	from    string
+	to      string
+	title   string
+	body    string
+	usrname string
+	pwd     string
+}
+
+func (m *Mail) Send(body string) error {
+	auth := smtp.PlainAuth("", m.usrname, m.pwd, "smtp.gmail.com")
+	if err := smtp.SendMail("smtp.gmail.com:587", auth, m.from, []string{m.to}, []byte(m.body)); err != nil {
+		return err
+	}
+	return nil
+}
