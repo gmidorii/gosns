abdb85b5a8b2a255fa0beb9b8bccc197bb26d3c3
tagasouichiro <stitow14@gmail.com>
2017-06-24 20:13:09 +0900

    Add decodeBody test

--- meta/hadler_test.go
+++ meta/hadler_test.go
@@ -1,1 +1,31 @@
 package meta
+
+import (
+	"bytes"
+	"net/http/httptest"
+	"testing"
+)
+
+type Sample struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func TestDecodeBody(t *testing.T) {
+	json := `{
+		"id" : 1,
+		"name" : "hoge"
+}`
+	req := httptest.NewRequest("POST", "/test", bytes.NewBufferString(json))
+	var s Sample
+	decodeBody(req, &s)
+
+	if s.ID != 1 {
+		t.Error("failed id")
+		t.Log("s.ID: " + string(s.ID) + ", id: 1")
+	}
+	if s.Name != "hoge" {
+		t.Error("failed name")
+		t.Log("s.Name: " + s.Name + ", Name: hoge")
+	}
+}
